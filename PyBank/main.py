ba92e184874a0381c5a54751ca26295c0a5953e1
KerryKova <kerrykovacik@gmail.com>
2018-11-04 12:13:57 -0600

    update to pybank code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,9 +1,22 @@
 import os
 import csv
 
-csvpath = os.path.join("..", "python-challenge", "pybank", "budget_data.csv")
+csvpath = os.path.join("budget_data.csv")
 
-with open(csvpath, "r", newline=" ") as pybank:
+with open(csvpath, "r") as pybank:
     csvreader = csv.reader(pybank, delimiter=",") 
 
-      
+    months = 0
+    profit_loss = 0
+
+    for i, row in enumerate(csvreader):
+        if i == 0:
+            header = row
+        else:
+            months = months + 1
+            profit_loss = profit_loss + int(row[1])
+            
+print("Financial Statement")
+print("----------------------------")
+print(f"Total Months: {months}")
+print(f"Total ${profit_loss}")
